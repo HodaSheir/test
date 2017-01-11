935daa6ac350a8a37e7e736a3c51fdb995843a30
HodaSheir <hoda.sheir@gmail.com>
2017-01-11 13:19:08 -0800

    fdfgfdg dfbgb

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+<body>
+    <div ng-app=""  ng-init="person=[1,1,5,475,545]">
+        <p> name is : <span ng-bind="person[3]"></span></p>
+    </div>
+</body>
+</html>
